6cde30a1609c351518ff2d390740c9de0a5a721b
JaishyamRB <jaishyam1998@gmail.com>
2024-10-20 00:33:46 -0400

    Major changes: Added product model and made bakend CRUD operations. Used chat GPT for frontend design

--- pizzashopapp/views.py
+++ pizzashopapp/views.py
@@ -4,6 +4,9 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Product
+
+context = {"Products":Product.objects.all()}
 
 # Create your views here.
 
@@ -21,15 +24,18 @@ class AdminLoginPage(generic.TemplateView):
         return render(request,'pizzashopapp/adminlogin.html')
     
     def post(self,request,*args,**kwargs):
-        #handles auntentication and redirection
-        username= request.POST['username']
-        password = request.POST['password']
+       
+       action = request.POST.get('action',None)
+       #handles auntentication and redirection
+       if not action:
+           username= request.POST['username']
+           password = request.POST['password']
 
-        user = authenticate(username = username, password = password)
-        if user:
+           user = authenticate(username = username, password = password)
+           if user:
             login(request,user)
             return redirect('pizzashopapp:adminHomepage')
-        else:
+           else:
             messages.add_message(request,messages.ERROR,"Invalid Credentials")
             return redirect('pizzashopapp:adminLoginPage')
 
@@ -38,4 +44,27 @@ class AdminHomePage(LoginRequiredMixin,generic.TemplateView):
     
     def get(self,request,*args,**kwargs):
         #handles redirecion based on authentication
-        return render(request,'pizzashopapp/adminhomepage.html')
+        return render(request,'pizzashopapp/adminhomepage.html',context)
+    
+    def post(self,request,*args,**kwargs):
+       action = request.POST.get('action_name')
+       if action == "add_product":
+          print(request.POST['name'],request.POST['price'])
+          product = Product(name = request.POST['name'], price = request.POST['price'])
+          product.save()
+          print("+++++",product,"+++++")
+        #   self.get(self,request,*args,**kwargs)
+       
+       elif action == "delete_product":
+        Product.objects.filter(id = request.POST.get('product_pk')).delete()
+
+       elif action == "edit_product":
+          current_product = Product.objects.filter(id = request.POST.get('product_pk'))[0]
+          current_product.name = request.POST.get('name')
+          current_product.price = request.POST.get('price')
+          current_product.save()
+
+       context = {"Products":Product.objects.all()}
+       return render(request,'pizzashopapp/adminhomepage.html',context)
+        
+        
